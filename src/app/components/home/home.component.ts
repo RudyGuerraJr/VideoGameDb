82ecd8b6e8996c4e55ee8261344f88e7d925f58a
RudyGuerraJr <rudyjr88@icloud.com>
2022-07-15 10:53:30 -0500

    description

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -25,7 +25,7 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  searchGames(sort: string, search?:string): void {
+  searchGames(sort: string, search?: string): void {
     this.httpService
     .getGameList(sort, search)
     .subscribe((gameList: APIResponse<Game>) => {
