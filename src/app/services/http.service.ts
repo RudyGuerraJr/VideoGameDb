8e9fff7142fe29142c13e99898d39ec98e6708d5
Megan <mwright@appddictionstudio.com>
2022-07-18 12:06:24 -0500

    html

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { APIResponse, Game } from '../models';
+import { forkJoin, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment as env } from 'src/environments/environment';
+import { APIResponse, Game } from '../models';
 
 @Injectable({
   providedIn: 'root'
@@ -13,20 +14,40 @@ export class HttpService {
 
   getGameList(
     ordering: string,
-    search?: string,
-  ):Observable<APIResponse<Game>> {
+    search?: string
+  ): Observable<APIResponse<Game>> {
     let params = new HttpParams().set('ordering', ordering);
 
     if (search) {
-      params= new HttpParams().set('ordering', ordering).set('search',search);
+      params = new HttpParams().set('ordering', ordering).set('search', search);
     }
 
-    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`,{
-      params:params,
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
+      params: params,
     });
+  }
 
+  getGameDetails(id: string): Observable<Game> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
+    const gameTrailersRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/movies`
+    );
+    const gameScreenshotsRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/screenshots`
+    );
 
-
-
+    return forkJoin({
+      gameInfoRequest,
+      gameScreenshotsRequest,
+      gameTrailersRequest,
+    }).pipe(
+      map((resp: any) => {
+        return {
+          ...resp['gameInfoRequest'],
+          screenshots: resp['gameScreenshotsRequest']?.results,
+          trailers: resp['gameTrailersRequest']?.results,
+        };
+      })
+    );
   }
 }
